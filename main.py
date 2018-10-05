c0fb00ce0cb0bf717c6c813892dc8e0892718852
Russ Jury <russ@securetunnel.com>
2018-10-05 00:31:07 -0500

    typo

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if app.config['SHOWHOST'] == "true":
 def index():
 
     # MySQL Connection
-    connection = mysql.connect(user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'], host=os.environ['MYSQL_HOST'], port=os.environ['MYSQL_PORT'], db=os.environ['MYSQL_DATABASE'], ssl={'ssl': {'ca': os.environ['MYSQL_SSL_CA']}})
+    connection = pymysql.connect(user=os.environ['MYSQL_USER'], password=os.environ['MYSQL_PASSWORD'], host=os.environ['MYSQL_HOST'], port=os.environ['MYSQL_PORT'], db=os.environ['MYSQL_DATABASE'], ssl={'ssl': {'ca': os.environ['MYSQL_SSL_CA']}})
     cursor = connection.cursor()
 
     # Vote tracking
